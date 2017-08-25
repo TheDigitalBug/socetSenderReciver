c18300de46848f40568f70cd6b6bdec6876f6525
TheDigitalBug <zemixx@bigmir.net>
2017-08-25 17:14:04 +0300

    fin fix

--- client.c
+++ client.c
@@ -64,23 +64,19 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 		errorMsg("USAGE: ./client IPv4 port fileToSend");
 	
-	getServerInfo(&socketAddr, argv[1], argv[2]); //get information about server
+	getServerInfo(&socketAddr, argv[1], argv[2]);
 	
 	if (((fd = open(argv[3], O_RDONLY)) < 0) || (read(fd, buf, 0) == -1))
 		errorMsg("Can't open file!");
 
-	// 1. Create socket for incoming connections
 	if ((clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
 		errorMsg("socket() failed");
 	
-	// 2. set IP/PORT of Server. CONNECT
 	if (connect(clientSocket, (struct sockaddr *) &socketAddr, sizeof(socketAddr)) < 0)
 		errorMsg("connect() failed");
 	printf(COLOR_GREEN"Connected to server [%s : %s]\n" COLOR_RESET, argv[1], argv[2]);
 	
 	sendFileName(clientSocket, argv[3]);
-	
-	// git file size
 	fileSize = getFileSize(fd);
 	
 	sendedTotal = 0;
@@ -92,13 +88,10 @@ int main(int argc, char *argv[])
 		 if (sended != (size_t)recv(clientSocket, buf, sended, 0))
 			 errorMsg("file wasn't send!");
 		printf("[%d | %zu bytes]\r", fileSize, sendedTotal += sended);
-		
-
 	}
-	
 	close(fd);
 	printf("\nsuccessfully send!\n");
 	printf(COLOR_RED"Disconnected from server [%s : %d] \n\n"COLOR_RESET, SERVER_IP, SERVER_PORT);
-	shutdown(clientSocket, SHUT_RDWR);
+	close(clientSocket);
 	return 0;
 }
